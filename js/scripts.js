62b5a0ebfbdbc08689e150652a4b6860f1cf0467
bendunhampdx <bendunhampdx@gmail.com>
2021-08-13 10:34:02 -0700

    add show/hide for results of survey, add grid with two columns to seperate survey from results

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
   $("form#language").submit(function(event) {
     event.preventDefault();
-    
+    const nameInput = $("input#person1").val();
+    const colorInput = $("#color").val();
+
+    $(".person1").text(nameInput);
+
+
+
+
+    $("body").css("background-color", colorInput);
+    $("#output").show();
   })
 })
