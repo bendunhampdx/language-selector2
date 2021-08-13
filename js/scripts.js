5586327647917e3dac561b4e8cc42003fdd92ed1
bendunhampdx <bendunhampdx@gmail.com>
2021-08-13 13:38:45 -0700

    add brancgin statements to account for any selection made

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,35 @@ $(document).ready(function() {
     event.preventDefault();
     const q1 = $("input#person1").val();
     const q2 = $("input:radio[name=day]:checked").val();
-    const q3 = $("input:radio[name=day]:checked").val();
-    const q4 = $("input:radio[name=day]:checked").val();
-    const q5 = $("input:radio[name=day]:checked").val();
+    const q3 = $("input:radio[name=season]:checked").val();
+    const q4 = $("input:radio[name=device]:checked").val();
+    const q5 = $("input:radio[name=caffeine]:checked").val();
     const q6 = $("#color").val();
 
 
-    if (q2 === 'morning') {
+    if (q2 === 'morning' && q3 === 'summer' && q4 === 'iOS' && q5 === 'severe') {
+      $('.div2').show();
+      $('#csharp').hide();
+      $('#javascript').hide();
       $('#ruby').show();
+      $('#language')[0].reset();
+    } else if (q2 === 'morning' && q3 === 'summer' && q4 === 'iOS' && q5 === 'moderate') {
+      $('.div2').show();
+      $('#ruby').hide();
+      $('#csharp').hide();
+      $('#javascript').show();
+      $('#language')[0].reset();
+    } else if (q2 === 'morning') {
+      $('.div2').show();
+      $('#ruby').hide();
+      $('#javascript').hide();
+      $('#csharp').show();
+      $('#language')[0].reset();
+    } else {
+      $('.div2').show();
+      $('#ruby').hide();
+      $('#javascript').hide();
+      $('#csharp').show();
       $('#language')[0].reset();
     }
 
