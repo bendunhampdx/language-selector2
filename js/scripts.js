b3c5986aad6141cf27ba12d0c28e4ce448fa638e
bendunhampdx <bendunhampdx@gmail.com>
2021-08-13 11:40:37 -0700

    add first branching statement and get it to hide/show on form submission

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,30 @@
 $(document).ready(function() {
   $("form#language").submit(function(event) {
     event.preventDefault();
-    const nameInput = $("input#person1").val();
-    const colorInput = $("#color").val();
+    const q1 = $("input#person1").val();
+    const q2 = $("input:radio[name=day]:checked").val();
+    const q3 = $("input:radio[name=day]:checked").val();
+    const q4 = $("input:radio[name=day]:checked").val();
+    const q5 = $("input:radio[name=day]:checked").val();
+    const q6 = $("#color").val();
 
-    $(".person1").text(nameInput);
+
+    if (q2 === 'morning') {
+      $('#javascript').show();
+    }
 
 
 
 
-    $("body").css("background-color", colorInput);
-    $("#output").show();
+    $(".person1").text(q1);
+    $(".day").text(q2);
+    $(".day").text(q3);
+    $(".day").text(q4);
+    $(".day").text(q5);
+
+
+
+    $("body").css("background-color", q6);
+    
   })
 })
